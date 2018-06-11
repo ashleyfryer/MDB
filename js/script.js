2282c825e1ce1bd1fe7a6cec011292424045e1ed
Riley Cravens <rlycrvns@gmail.com>
2018-06-11 14:48:53 -0700

    added genre translation

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ function getInput() {
 
 function clearLists () {
     var recommendedList = $('#recommended');
-    
+
     recommendedList.text(' ');
 }
 
@@ -26,16 +26,16 @@ function nowPlaying() {
     console.log(xhr.status); // if this returns 200, then you are in like Flynn
     var moviePopular = JSON.parse(xhr.responseText);
     console.log(moviePopular);
-    
+
     for (x=0; x < 5; x++) {
         var popular = $("#nowplaying");
-        
+
         var image = '<img src="' +  imageurlsmall + moviePopular.results[x].poster_path + '">';
-        
+
         console.log (moviePopular.results[x].original_title);
-        
+
         var nowPlaying = popular.append("<a href='#' onclick='getMovie(\"" + moviePopular.results[x].original_title + "\")'>" + '<p class="col span_2_of_12">' + moviePopular.results[x].original_title + image + '</p>' + "</a>");
-        
+
     }
 }
 
@@ -48,17 +48,17 @@ function recommendMovie(movie) {
     xhr.send();
     console.log(xhr.status); // if this returns 200, then you are in like Flynn
     var movieDiscover = JSON.parse(xhr.responseText);
-    
+
     clearLists();
-    
+
     for (x=0; x < 5; x++) {
         var discover = $("#recommended");
-        
+
         var image = '<img src="' +  imageurlsmall + movieDiscover.results[x].poster_path + '">';
-        
+
         var recommend = discover.append("<a href='#' onclick='getMovie(\"" + movieDiscover.results[x].original_title + "\")'>" +'<p class="col span_2_of_12">' + movieDiscover.results[x].original_title + image + '</p>' + "</a>");
 
-    }   
+    }
 }
 
 function getMovie(input) {
@@ -77,19 +77,81 @@ function getMovie(input) {
     $("#overview").text(movieObject.results["0"].overview);
     $("#language").text(movieObject.results["0"].original_language);
     $("#popularity").text(movieObject.results["0"].vote_average);
-    $("#genre").text(movieObject.results["0"].genre_ids);
-    
+    $("#genre").text("");
+    var genre = movieObject.results["0"].genre_ids;
+    for (var c = 0; c < genre.length; c++) {
+    switch(genre[c]) {
+      case 28:
+        $("#genre").append("Action ");
+        break;
+      case 12:
+        $("#genre").append("Adventure ");
+        break;
+      case 16:
+        $("#genre").append("Animation ");
+        break;
+      case 35:
+        $("#genre").append("Comedy ");
+        break;
+      case 80:
+        $("#genre").append("Crime ");
+        break;
+      case 99:
+        $("#genre").append("Documentary ");
+        break;
+      case 18:
+        $("#genre").append("Drama ");
+        break;
+      case 10751:
+        $("#genre").append("Family ");
+        break;
+      case 14:
+        $("#genre").append("Fantasy ");
+        break;
+      case 36:
+        $("#genre").append("History ");
+        break;
+      case 27:
+        $("#genre").append("Horror ");
+        break;
+      case 10402:
+        $("#genre").append("Music ");
+        break;
+      case 9648:
+        $("#genre").append("Mystery ");
+        break;
+      case 10749:
+        $("#genre").append("Romance ");
+        break;
+      case 878:
+        $("#genre").append("Science Fiction ");
+        break;
+      case 10770:
+        $("#genre").append("TV Movie ");
+        break;
+      case 53:
+        $("#genre").append("Thriller ");
+        break;
+      case 10752:
+        $("#genre").append("War ");
+        break;
+      case 37:
+        $("#genre").append("Western ");
+        break;
+      }
+    }
+
     for (x=0; x < 5; x++) {
         var options = $(".otherMovies");
-        
+
         var recommend = options.append("<a href='#' onclick='getMovie(\"" + movieObject.results[x].original_title + "\")'>" +'<li>' + movieObject.results[x].original_title + '</li>' + "</a>");
 
     }
     recommendMovie(movieObject);
-    
+
 }
 
-//Hides "Not the right movie?" and alternate movie options 
+//Hides "Not the right movie?" and alternate movie options
 //unless you search for something
 
 
